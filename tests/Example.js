5da76d295ac08af61e6f102c3a84668bd4450565
VOoDOoEvil <kubis.honza@gmail.com>
2017-04-05 12:28:25 +0200

    oprava 2

--- tests/Example.js
+++ tests/Example.js
@@ -3,11 +3,9 @@ var URL = 'https://www.google.cz/?gfe_rd=cr&ei=5cTkWNrPIcnV8gf0xIawBA';
 var searchBar = '#lst-ib';
 var vyhledat = '#_fZl';
 
-
 module.exports = {
 
-
-  'Login - Spatne udaje' : function (client) {
+  'Vyhledat neco v google search' : function (client) {
     client
       .url(URL)
       .setValue(searchBar, 'goooooooglleeeee')
